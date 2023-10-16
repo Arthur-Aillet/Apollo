12f2452e638ea227376de31dbabfe75005418db4
Arthur-Aillet <arthur.aillet@epitech.eu>
2023-10-16 10:39:53 +0200

    feat(CI): unit tests in place

---
+++ test/ParserSpec.hs
@@ -0,0 +1,14 @@
+module ParserSpec (parserTests) where
+import Test.HUnit
+
+parserTests :: Test
+parserTests =
+  TestList
+    [ "example" ~: exampleTests
+    ]
+
+exampleTests :: Test
+exampleTests =
+  TestList
+    [ "Test 1" ~: True @=? True
+    ]
