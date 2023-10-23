cd6329c4f1db7ec9f2c04571f7e6bc449c131d3e
syborg64 <julian.scott@epitech.eu>
2023-10-23 14:15:32 +0200

    feat(AST): AST proposition

--- src/Ast/Type.hs
+++ src/Ast/Type.hs
@@ -7,7 +7,43 @@
 
 module Ast.Type (Ast(..)) where
 
+import Atom.Atom
+
+data Function = Function [(String, Operable)] Ast
+
+data Definition
+  =
+    FuncDefinition String Function -- define a function
+  | VarDefinition String Operable -- define a variable
+
+data Structure -- layout, structure and connection of statements, having no value
+  =
+    Resolved -- expression resolving to no value
+  | If Operable Ast Ast -- branching condition (if (x) {} {})
+  | Single Ast -- single operation or operable ({x})
+  | Block [Ast] [String] -- several actions ordered by variable precedence ({x;y})
+  | Sequence [Ast] -- several actions ordered by precedence ({x >> y})
+  deriving (Show, Eq)
+
+data Operation -- statement involving an action, resulting in a value
+  =
+    Interrupt String -- Interrupt program flow
+  | CallStd String [Operable] -- call a standard or builtin operation (x(y))
+  | CallFunc String [Operable] -- call a function, exposes both inherent IOPipes (x(y))
+  | CallSH String [Operable] -- syscall of builtin program ($x(y)), exposes both IOPipes
+  | Pipe Operable Operable -- stdout mapped to stdin ({x.y}, {x <- y})
+  deriving (Show, Eq)
+
+data Operable -- statement having a value
+  =
+    Variable String -- Variable reffering to single known value
+  | Value Atom -- Single known value
+  | Operation Operation -- operation resulting in an operable value
+  | IOPipe String -- named pipe, String is likely a placeholder
+  deriving (Show, Eq)
+
 data Ast
-  = Truth Bool -- Single known boolean value
-  | If Ast Ast Ast -- branching condition
+  =
+    Structure Structure -- structure block ({})
+  | Operable Operable -- inherent value (x)
   deriving (Show, Eq)
