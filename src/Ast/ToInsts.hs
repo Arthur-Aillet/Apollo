2b4df4e6962df9f3caab452d3e2113720fb9754e
Arthur-Aillet <arthur.aillet@epitech.eu>
2023-10-25 02:46:47 +0200

    feat: Either error all the way in compiling

--- src/Ast/ToInsts.hs
+++ src/Ast/ToInsts.hs
@@ -9,13 +9,13 @@ module Ast.ToInsts (module Ast.ToInsts) where
 
 import Ast.Type
 import Atom.Atom (Atom (AtomI))
-import Data.HashMap.Lazy (HashMap, empty, fromList, insert, (!?))
+import Data.HashMap.Lazy (HashMap, empty, fromList, fromListWith, insert, (!?))
 import Debug.Trace
 import Eval.Builtin
 
 newtype Context = Context (HashMap String (Int, Function))
 
-data LocalContext = LocalContext (HashMap String Type) (Maybe Type)
+data LocalContext = LocalContext (HashMap String (Int, Type)) (Maybe Type)
 
 data Binary = Binary Env Func deriving (Show)
 
@@ -53,41 +53,61 @@ toInsts defs = case createCtx defs (Context empty) 0 of
   Right ctx -> convAllFunc defs (Binary [] []) ctx
 
 convAllFunc :: [Definition] -> Binary -> Context -> Either String Binary
-convAllFunc ((FuncDefinition "main" func) : xs) (Binary env []) ctx =
-  convAllFunc xs (Binary env (convFunc func ctx)) ctx
+convAllFunc ((FuncDefinition "main" func) : xs) (Binary env []) ctx = case convFunc func ctx of
+  Left err -> Left err
+  Right func -> convAllFunc xs (Binary env func) ctx
 convAllFunc ((VarDefinition _ _) : _) _ _ = Left "Error: Global Variables not supported yet"
-convAllFunc ((FuncDefinition _ (Function args y z)) : xs) (Binary env funcs) ctx =
-  convAllFunc xs (Binary (env ++ [(length args, convFunc (Function args y z) ctx)]) funcs) ctx
+convAllFunc ((FuncDefinition _ (Function args y z)) : xs) (Binary env funcs) ctx = case convFunc (Function args y z) ctx of
+  Left err -> Left err
+  Right func -> convAllFunc xs (Binary (env ++ [(length args, func)]) funcs) ctx
 convAllFunc [] bin _ = Right bin
 
-createLocalContext :: [(String, Type)] -> Maybe Type -> LocalContext
-createLocalContext args = LocalContext (fromList args)
+(+++) :: [a] -> [a] -> [a] -> [a]
+(+++) a b c = a ++ b ++ c
 
-convFunc :: Function -> Context -> Insts
-convFunc (Function args output ast) ctx = convAst ast ctx local
-  where
-    local = createLocalContext args output
+(++++) :: [a] -> [a] -> [a] -> [a] -> [a]
+(++++) a b c d = a ++ b ++ c ++ d
 
-convStruct :: Structure -> Context -> LocalContext -> Insts
-convStruct Resolved _ _ = []
-convStruct (Return a) _ _ = []
+attachIndex :: [(String, Type)] -> Int -> [(String, (Int, Type))]
+attachIndex [] _ = []
+attachIndex ((str, t) : xs) acc = (str, (acc, t)) : attachIndex xs (acc + 1)
+
+createLocalContext :: [(String, Type)] -> Maybe Type -> Either String LocalContext
+createLocalContext args output = Right $ LocalContext (fromList (attachIndex args 0)) output
+
+convFunc :: Function -> Context -> Either String Insts
+convFunc (Function args output ast) ctx = case createLocalContext args output of
+  Left err -> Left err
+  Right local -> convAst ast ctx local
+
+convStruct :: Structure -> Context -> LocalContext -> Either String Insts
+convStruct Resolved _ _ = Left "Err: unsupported"
+convStruct (Return a) _ _ = Left "Err: unsupported"
 convStruct (If op ast_then ast_else) c l =
-    opera
-    ++ jump
-    ++ then_insts
-    ++ else_insts
+  (++++)
+    <$> opera
+    <*> jump
+    <*> then_insts
+    <*> else_insts
   where
     opera = convOp op c l
-    jump = [JumpIfFalse (length then_insts)]
+    jump = Right [JumpIfFalse (length then_insts)]
     then_insts = convAst ast_then c l
     else_insts = convAst ast_else c l
-convStruct (Single ast) _ _ = []
-convStruct (Block asts vars) _ _ = []
-convStruct (Sequence asts) _ _ = []
+convStruct (Single ast) _ _ = Left "Err: unsupported"
+convStruct (Block asts vars) _ _ = Left "Err: unsupported"
+convStruct (Sequence asts) _ _ = Left "Err: unsupported"
 
-convOp :: Operable -> Context -> LocalContext -> Insts
-convOp _ _ _ = [Push (Int 0)]
+convOp :: Operable -> Context -> LocalContext -> Either String Insts
+convOp _ _ _ = Right [Push (Int 0)]
 
-convAst :: Ast -> Context -> LocalContext -> Insts
+{-
+  = OpVariable String -- Variable reffering to single known value
+  | OpValue Atom -- Single known value
+  | OpOperation Operation -- operation resulting in an operable value
+  | OpIOPipe String --
+  -}
+
+convAst :: Ast -> Context -> LocalContext -> Either String Insts
 convAst (AstStructure struct) c l = convStruct struct c l
-convAst (AstOperation op) _ _ = []
+convAst (AstOperation op) _ _ = Left "Err: unsupported"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,7 @@
+import AtomSpec (atomTests)
 import Control.Monad
+import ParserSpec (parserTests)
 import Test.HUnit
-import ParserSpec(parserTests)
-import AtomSpec(atomTests)
 
 main :: IO ()
 main = Control.Monad.void (runTestTT tests)
@@ -9,7 +9,6 @@ main = Control.Monad.void (runTestTT tests)
 tests :: Test
 tests =
   TestList
-    [
-      parserTests,
+    [ parserTests,
       atomTests
     ]
