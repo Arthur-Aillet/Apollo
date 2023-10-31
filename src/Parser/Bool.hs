402d689cbabb70c4971782dbe7d271b1ec79c5e9
Ludofr3 <ludovic.de-chavagnac@epitech.eu>
2023-10-31 17:29:04 +0100

    fix: syntaxe parseBool

--- src/Parser/Bool.hs
+++ src/Parser/Bool.hs
@@ -12,4 +12,4 @@ import Parser.Symbol (parseSymbol)
 import Parser.Type (Parser (..))
 
 parseBool :: Parser Bool
-parseBool = (== "True") <$> (parseSymbol "True" <|> parseSymbol "False")
+parseBool = (== "true") <$> (parseSymbol "true" <|> parseSymbol "false")
