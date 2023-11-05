23a88a5bce1e5ce04e55153e32602bc2e399cb55
Ludofr3 <ludovic.de-chavagnac@epitech.eu>
2023-11-05 12:56:07 +0100

    test: unit test for Position.hs, Char.hs, Symbol.hs, Bool.hs in Parser/

---
+++ test/Parser/CharSpec.hs
@@ -0,0 +1,94 @@
+{-
+-- EPITECH PROJECT, 2023
+-- Dev_repo2
+-- File description:
+-- CharSpec.hs
+-}
+
+module Parser.CharSpec (charTests) where
+
+import Test.HUnit
+import Parser.PositionSpec(getPosition)
+import Parser.Position (defaultPosition)
+import Parser.Range (defaultRange)
+import Parser.StackTrace (StackTrace (..), defaultLocation)
+import Parser.Type (Parser (..))
+
+import Parser.Char
+
+charTests :: Test
+charTests =
+    TestList
+    [   "parseAChar" ~: parseACharTests,
+        "parseDigit" ~: parseDigitTests,
+        "parseOpeningAndClosing Functions" ~: parseOpeningAndClosingTests,
+        "parseChar" ~: parseCharTests,
+        "parseNotChar" ~: parseNotCharTests,
+        "parseAnyChar" ~: parseAnyCharTests,
+        "parseNotAnyChar" ~: parseNotAnyCharTests
+    ]
+
+parseACharTests :: Test
+parseACharTests =
+  TestList
+    [ "delete the first character '\n'" ~: (Right ('\n', "hello world!", (getPosition 0 1))) @=? (runParser parseAChar "\nhello world!" defaultPosition),
+      "delete the first character 'h'" ~: (Right ('h', "ello world!", (getPosition 1 0))) @=? (runParser (parseChar 'h') "hello world!" defaultPosition),
+      "Error '\n'" ~: (Left (StackTrace [("Not Found: End of Input", defaultRange, defaultLocation)])) @=? (runParser parseAChar "" defaultPosition)
+    ]
+
+parseDigitTests :: Test
+parseDigitTests =
+    TestList
+    [   "is digit" ~: (Right ('9', "5678", (getPosition 1 0))) @=? (runParser parseDigit "95678" defaultPosition),
+        "is not digit" ~: (Left (StackTrace [("Not Found: List is empty", defaultRange, defaultLocation)])) @=? (runParser parseDigit "foobar" defaultPosition),
+        "Empty" ~: (Left (StackTrace [("Not Found: List is empty", defaultRange, defaultLocation)])) @=? (runParser parseDigit "foobar" defaultPosition)
+    ]
+
+parseOpeningAndClosingTests :: Test
+parseOpeningAndClosingTests =
+    TestList
+    [ "Error parseOpeningQuote" ~: (Left (StackTrace [("Not Found: Missing opening Quote", defaultRange, defaultLocation)])) @=? (runParser parseOpeningQuote "foobar" defaultPosition),
+      "Error parseClosingQuote" ~: (Left (StackTrace [("Not Found: Missing closing Quote", defaultRange, defaultLocation)])) @=? (runParser parseClosingQuote "foobar" defaultPosition),
+      "Error parseOpeningsQuote" ~: (Left (StackTrace [("Not Found: Missing opening Quote", defaultRange, defaultLocation)])) @=? (runParser parseOpeningsQuote "foobar" defaultPosition),
+      "Error parseClosingsQuote" ~: (Left (StackTrace [("Not Found: Missing closing Quote", defaultRange, defaultLocation)])) @=? (runParser parseClosingsQuote "foobar" defaultPosition),
+      "Error parseOpeningParenthesis" ~: (Left (StackTrace [("Not Found: Missing opening Parenthesis", defaultRange, defaultLocation)])) @=? (runParser parseOpeningParenthesis "foobar" defaultPosition),
+      "Error parseClosingParenthesis" ~: (Left (StackTrace [("Not Found: Missing closing Parenthesis", defaultRange, defaultLocation)])) @=? (runParser parseClosingParenthesis "foobar" defaultPosition),
+      "Error parseOpeningCurlyBraquet" ~: (Left (StackTrace [("Not Found: Missing opening curlybraquet", defaultRange, defaultLocation)])) @=? (runParser parseOpeningCurlyBraquet "foobar" defaultPosition),
+      "Error parseClosingCurlyBraquet" ~: (Left (StackTrace [("Not Found: Missing closing curlybraquet", defaultRange, defaultLocation)])) @=? (runParser parseClosingCurlyBraquet "foobar" defaultPosition),
+      "Error parseOpeningBraquet" ~: (Left (StackTrace [("Not Found: Missing opening braquet", defaultRange, defaultLocation)])) @=? (runParser parseOpeningBraquet "foobar" defaultPosition),
+      "Error parseClosingBraquet" ~: (Left (StackTrace [("Not Found: Missing closing braquet", defaultRange, defaultLocation)])) @=? (runParser parseClosingBraquet "foobar" defaultPosition)
+    ]
+
+parseCharTests :: Test
+parseCharTests =
+  TestList
+    [ "Test 1" ~: (Right ('\n', "hello world!", (getPosition 0 1))) @=? (runParser (parseChar '\n') "\nhello world!" defaultPosition),
+      "Test 2" ~: (Right ('h', "ello world!", (getPosition 1 0))) @=? (runParser (parseChar 'h') "hello world!" defaultPosition),
+      "Test 3" ~: (Left (StackTrace [("Not Found: charactere is not '\n' (is 'h')", defaultRange, defaultLocation)])) @=? (runParser (parseChar '\n') "hello world!" defaultPosition)
+    ]
+
+parseNotCharTests :: Test
+parseNotCharTests =
+  TestList
+    [ "Test 1" ~: (Left (StackTrace [("Not Found: character is '\n'", defaultRange, defaultLocation)])) @=? (runParser (parseNotChar '\n') "\nhello world!" defaultPosition),
+      "Test 2" ~: (Left (StackTrace [("Not Found: character is 'h'", defaultRange, defaultLocation)])) @=? (runParser (parseNotChar 'h') "hello world!" defaultPosition),
+      "Test 3" ~: (Right ('h', "ello world!", getPosition 1 0)) @=? (runParser (parseNotChar '\n') "hello world!" defaultPosition)
+    ]
+
+parseAnyCharTests :: Test
+parseAnyCharTests =
+  TestList
+    [ "Check if first char is in '\nc'" ~: (Right ('c', "oucou\n", (getPosition 1 0))) @=? (runParser (parseAnyChar "\nc") "coucou\n" defaultPosition),
+      "Check if first char is in 'c\n'" ~: (Right ('c', "oucou\n", (getPosition 1 0))) @=? (runParser (parseAnyChar "c\n") "coucou\n" defaultPosition),
+      "Check if first char is in 'rem'" ~: (Left (StackTrace [("Not Found: List is empty", defaultRange, defaultLocation)])) @=? (runParser (parseAnyChar "rem") "zero\n" defaultPosition),
+      "Check if first char is in ''" ~: (Left (StackTrace [("Not Found: List is empty", defaultRange, defaultLocation)])) @=? (runParser (parseAnyChar "") "zero\n" defaultPosition)
+    ]
+
+parseNotAnyCharTests :: Test
+parseNotAnyCharTests =
+  TestList
+    [ "Not any char of the string" ~: (Right ('f', "oo\n", (getPosition 1 0))) @=? (runParser (parseNotAnyChar "abcd") "foo\n" defaultPosition),
+      "Is in the string" ~: (Left (StackTrace [("Not Found: character is 'b'", defaultRange, defaultLocation)])) @=? (runParser (parseNotAnyChar "abcd") "bar" defaultPosition),
+      "String is empty" ~: (Left (StackTrace [("Not Found: List is empty", defaultRange, defaultLocation)])) @=? (runParser (parseNotAnyChar "") "zero\n" defaultPosition)
+    ]
+
---
+++ test/Parser/SymbolSpec.hs
@@ -0,0 +1,97 @@
+{-
+-- EPITECH PROJECT, 2023
+-- Dev_repo2
+-- File description:
+-- SymbolSpec.hs
+-}
+
+module Parser.SymbolSpec (symbolTests) where
+
+import Test.HUnit
+import Parser.PositionSpec(getPosition)
+import Parser.Position (defaultPosition)
+import Parser.Range (defaultRange)
+import Parser.StackTrace (StackTrace (..), defaultLocation)
+import Parser.Type (Parser (..))
+import Parser.Range (Range (..))
+import Ast.Ast (Type (..))
+
+import Parser.Symbol
+
+symbolTests :: Test
+symbolTests =
+    TestList
+    [
+        "parseSymbol" ~: parseSymbolTests,
+        "goodType" ~: goodTypeTests,
+        "parseListType" ~: parseListTypeTests,
+        "parseSymbolType" ~: parseSymbolTypeTests,
+        "parseType" ~: parseTypeTests,
+        "parseMaybeType" ~: parseMaybeTypeTests
+    ]
+
+parseSymbolTests :: Test
+parseSymbolTests =
+  TestList
+    [ "Test 1" ~: (Right ("azerty", " hello", (getPosition 6 0))) @=? (runParser (parseSymbol "azerty") "azerty hello" defaultPosition),
+      "Test 2" ~: (Left (StackTrace [("Not Found: charactere is not 'e' (is 'a')", defaultRange, defaultLocation)])) @=? (runParser (parseSymbol "ezryta") "azerty hello" defaultPosition),
+      "Test 3" ~: (Left (StackTrace [("Not Found: End of Input", defaultRange, defaultLocation)])) @=? (runParser (parseSymbol "azerty") "" defaultPosition)
+    ]
+
+goodTypeTests :: Test
+goodTypeTests =
+    TestList
+    [ "Type valide (TypeInt)" ~: (Right (TypeInt, "int", defaultPosition)) @=? (runParser (isgoodType (pure (Just (TypeInt)))) "int" defaultPosition),
+      "Type valide (TypeString)" ~: (Right (TypeString, "string", defaultPosition)) @=? (runParser (isgoodType (pure (Just TypeString))) "string" defaultPosition),
+      "Type valide (TypeBool)" ~: (Right (TypeBool, "bool", defaultPosition)) @=? (runParser (isgoodType (pure (Just TypeBool))) "bool" defaultPosition),
+      "Type valide (TypeChar)" ~: (Right (TypeChar, "char", defaultPosition)) @=? (runParser (isgoodType (pure (Just TypeChar))) "char" defaultPosition),
+      "Type valide (TypeList TypeInt)" ~: (Right (TypeList (Just TypeInt), "[int]", defaultPosition)) @=? (runParser (isgoodType (pure (Just (TypeList (Just TypeInt))))) "[int]" defaultPosition),
+      "Type valide (TypeList TypeString)" ~: (Right (TypeList (Just TypeString), "[string]", defaultPosition)) @=? (runParser (isgoodType (pure (Just (TypeList (Just TypeString))))) "[string]" defaultPosition),
+      "Type valide (TypeList (TypeList TypeChar))" ~: (Right (TypeList (Just (TypeList (Just TypeChar))), "[[char]]", defaultPosition)) @=? (runParser (isgoodType (pure (Just (TypeList (Just (TypeList  (Just TypeChar))))))) "[[char]]" defaultPosition),
+      "Type invalide (Renvoie Nothing)" ~: (Left (StackTrace [("This type doesn't exist: ", defaultRange, defaultLocation)])) @=? (runParser (isgoodType (pure Nothing)) "invalid" defaultPosition)
+    ]
+
+parseListTypeTests :: Test
+parseListTypeTests =
+    TestList
+    [ "TypeList (TypeInt)" ~: (Right ("[int]", "", getPosition 5 0)) @=? (runParser parseListType "[int]" defaultPosition),
+      "TypeList (TypeString)" ~: (Right ("[string]", "", getPosition 8 0)) @=? (runParser parseListType "[string]" defaultPosition),
+      "TypeList (TypeBool)" ~: (Right ("[bool]", "", getPosition 6 0)) @=? (runParser parseListType "[bool]" defaultPosition),
+      "Type invalide (TypeList)" ~: (Left (StackTrace [("Not Found: charactere is not 't' (is 'v')", Range (getPosition 3 0) (getPosition 3 0), defaultLocation)])) @=? (runParser parseListType "[invalid]" defaultPosition),
+      "Type invalide (Symbole manquant)" ~: (Left (StackTrace [("Not Found: Missing closing braquet", Range (getPosition 4 0) (getPosition 4 0), defaultLocation)])) @=? (runParser parseListType "[int" defaultPosition)
+  ]
+
+parseSymbolTypeTests :: Test
+parseSymbolTypeTests =
+    TestList
+    [ "Type valide (TypeInt)" ~: (Right ("int", "", getPosition 3 0)) @=? (runParser parseSymbolType "int" defaultPosition),
+      "Type valide (TypeString)" ~: (Right ("string", "", getPosition 6 0)) @=? (runParser parseSymbolType "string" defaultPosition),
+      "Type valide (TypeBool)" ~: (Right ("bool", "", getPosition 4 0)) @=? (runParser parseSymbolType "bool" defaultPosition),
+      "Type valide (TypeChar)" ~: (Right ("char", "", getPosition 4 0)) @=? (runParser parseSymbolType "char" defaultPosition),
+      "Type valide (TypeList)" ~: (Right ("[int]", "", getPosition 5 0)) @=? (runParser parseSymbolType "[int]" defaultPosition),
+      "Type invalide" ~: (Left (StackTrace [("Not Found: charactere is not 't' (is 'v')", Range (getPosition 2 0) (getPosition 2 0), defaultLocation)])) @=? (runParser parseSymbolType "invalid" defaultPosition)
+    ]
+
+parseTypeTests :: Test
+parseTypeTests =
+    TestList
+  [
+      "Type valide (TypeInt)" ~: (Right (TypeInt, "", getPosition 3 0)) @=? (runParser parseType "int" defaultPosition),
+      "Type valide (TypeString)" ~: (Right (TypeList (Just TypeChar), "", getPosition 6 0)) @=? (runParser parseType "string" defaultPosition),
+      "Type valide (TypeBool)" ~: (Right (TypeBool, "", getPosition 4 0)) @=? (runParser parseType "bool" defaultPosition),
+      "Type valide (TypeChar)" ~: (Right (TypeChar, "", getPosition 4 0)) @=? (runParser parseType "char" defaultPosition),
+      "Type valide (TypeList)" ~: (Right ((TypeList (Just TypeInt)), "", getPosition 5 0)) @=? (runParser parseType "[int]" defaultPosition),
+      "Type invalide" ~: (Left (StackTrace [("Not Found: charactere is not 't' (is 'v')", Range (getPosition 2 0) (getPosition 2 0), defaultLocation)])) @=? (runParser parseType "invalid" defaultPosition)
+  ]
+
+parseMaybeTypeTests :: Test
+parseMaybeTypeTests =
+    TestList
+  [
+      "Type valide (TypeInt)" ~: (Right (Just TypeInt, "", getPosition 3 0)) @=? (runParser parseMaybeType "int" defaultPosition),
+      "Type valide (TypeString)" ~: (Right (Just (TypeList (Just TypeChar)), "", getPosition 6 0)) @=? (runParser parseMaybeType "string" defaultPosition),
+      "Type valide (TypeBool)" ~: (Right (Just TypeBool, "", getPosition 4 0)) @=? (runParser parseMaybeType "bool" defaultPosition),
+      "Type valide (TypeChar)" ~: (Right (Just TypeChar, "", getPosition 4 0)) @=? (runParser parseMaybeType "char" defaultPosition),
+      "Type valide (TypeList)" ~: (Right (Just (TypeList (Just TypeInt)), "", getPosition 5 0)) @=? (runParser parseMaybeType "[int]" defaultPosition),
+      "Type invalide" ~: (Right (Nothing, "invalid", defaultPosition)) @=? (runParser parseMaybeType "invalid" defaultPosition)
+  ]
